affa86d74ac174248a5bc33f6823abf51484032b
mayajlee <mlee15@syr.edu>
2023-12-12 17:28:46 -0500

    Deleted Duplicated files, added update json file functionality and addUser option for register page.

--- TeamProject/Auth.java
+++ TeamProject/Auth.java
@@ -3,16 +3,20 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.*;
 
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.nio.file.Paths;
+import java.io.FileWriter;
+
 
 
 public class Auth {
     //read auth file
     public JSONArray users;
+    public String auPATH;
 
     public Auth(String authPath){
         Object parserObject = null;
+        auPATH = authPath;
         try {
             parserObject = new JSONParser().parse(new FileReader(authPath));
         } catch (IOException e) {
@@ -41,50 +45,98 @@ public class Auth {
         return "done";
     }
 
-    public void authorizationCheck (String user, String password, String docRequest) {
-        String authLvl;
-        String docName = docRequest;
+    public void addUser(String username, String password){
+       JSONObject newUser = new JSONObject();
 
-        // match User & password
-        verifyaccount(user, password);
-        String authLvl = account["authLvl"];
-
-        // Check if document exists first
-        find(docName);
-        String docClass = document["classification"];
-
-
-
-        // check if authorization allows them to access it
-        boolean clearance = authorizationCheck(userAuth, docClass);
-
-        if (clearance) {
-            // fulfill request, try to get file and send to user
-        }
-        else {
-            // send rejection header for "Not Authorized for request"
-        }
-
+       newUser.put("username", username);
+       newUser.put("password", password);
+       newUser.put("authLvl", "General"); //Default
+        users.add(newUser);
     }
 
-    public boolean authorizationCheck(String userAuth, String docClass) {
-        switch (userAuth) {
-            case "TopSecret":
-                if (docClass.equals("TopSecret")) return true;
-            case "Secret":
-                if (docClass.equals("Secret")) return true;
-            case "General":
-                return true;
-            break;
-            default:
-                return false;
+//    public void authorizationCheck (String user, String password, String docRequest) {
+//        String authLvl;
+//        String docName = docRequest;
+//
+//        // match User & password
+//        verifyaccount(user, password);
+//        String authLvl = account["authLvl"];
+//
+//        // Check if document exists first
+//        find(docName);
+//        String docClass = document["classification"];
+//
+//
+//
+//        // check if authorization allows them to access it
+//        boolean clearance = authorizationCheck(userAuth, docClass);
+//
+//        if (clearance) {
+//            // fulfill request, try to get file and send to user
+//        }
+//        else {
+//            // send rejection header for "Not Authorized for request"
+//        }
+//
+//    }
+//
+//    public boolean authorizationCheck(String userAuth, String docClass) {
+//        switch (userAuth) {
+//            case "TopSecret":
+//                if (docClass.equals("TopSecret")) return true;
+//            case "Secret":
+//                if (docClass.equals("Secret")) return true;
+//            case "General":
+//                return true;
+//            break;
+//            default:
+//                return false;
+//        }
+//    }
+    public void shutDown() throws FileNotFoundException {
+        File file = new File(Paths.get(auPATH).toString());
+        file.setWritable(true);
+        BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(new FileOutputStream(Paths.get(auPATH).toString()));
+        try {
+            bufferedOutputStream.write("".getBytes());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            bufferedOutputStream.flush();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        JSONObject updatedUsers = new JSONObject();
+        updatedUsers.put("users", users);
+
+        try {
+            bufferedOutputStream.write(updatedUsers.toJSONString().getBytes());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            bufferedOutputStream.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
-    //test and example of how to use -- NEED TO SOLVE JSON PROBLEM
+
     public static void main(String[] args) {
         Auth au = null;
-        au = new Auth("src/main/java/test.json");
+        au = new Auth("authorization.json");
         System.out.println(au.authenticate("sara123", "abc123"));
+
+        au.addUser("meep", "moop");
+        System.out.println(au.authenticate("meep", "moop"));
+
+        try {
+            au.shutDown();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
 
